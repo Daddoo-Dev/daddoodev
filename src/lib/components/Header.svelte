79a07427d1882ba8519a115e00a69ae888e0cc52
shawnmcpeek <shawnmcpeek@gmail.com>
2026-04-13 17:14:34 -0600

    remove MarketMinder link from Header component; streamline navigation menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,8 +6,7 @@
     { label: 'About', href: '#about' },
     { label: 'Projects', href: '#projects' },
     { label: 'Contact', href: '#contact' },
-    { label: 'QR Generator', href: '/qr-generator' },
-    { label: 'MarketMinder', href: '/marketminder' }
+    { label: 'QR Generator', href: '/qr-generator' }
   ];
 
   let isMenuOpen = false;
